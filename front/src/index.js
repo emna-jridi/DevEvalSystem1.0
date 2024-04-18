e0ef1c964ba73e05403767882e3f5da6984e8516
emna <emna.jridi@esen.tn>
2024-04-18 12:38:33 +0100

    Update filtering functionality

--- front/src/index.js
+++ front/src/index.js
@@ -25,8 +25,8 @@ import { from } from "stylis";
 import { LoadingProvider } from "./layouts/tables/LoadingContext";
 const container = document.getElementById("app");
 const root = createRoot(container);
-//axios.defaults.baseURL = "https://devevalsystemback.onrender.com";
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://devevalsystemback.onrender.com";
+//axios.defaults.baseURL = "http://localhost:4000";
 
 root.render(
   <LoadingProvider>
